6308df68518e26a549156a0291eb7436ee2955fb
Michel Basteiro <michelhfb2006@gmail.com>
2024-02-07 09:21:25 -0300

    Aprendendo operadores em JS

---
+++ JS/2_OPERADORES/js/script.js
@@ -0,0 +1,67 @@
+// // 1 - Number
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -128);
+
+// // 2 - Ops. aritméticas
+// console.log(2+4);
+// console.log(10-5);
+// console.log(5*7);
+// console.log(36 / 2);
+
+// // 3 - Special Numbers
+// console.log(typeof Infinity);
+// console.log(typeof -Infinity);
+// console.log(12 * "asd");
+// console.log(typeof NaN);
+
+// // 4 - Strings
+// console.log("Um texto");
+// console.log(`Outro texto`);
+// console.log('Mais um texto')
+
+// console.log(typeof "Um texto");
+// console.log(typeof 'Mais um texto');
+
+// 5 - Simbolos especiais em strings
+// console.log(typeof `Outro texto`);
+// console.log(`Testando a \nquebra de linha`);
+// console.log(`Espaçamento \t de tab`);
+
+// // 6 - Concatenação
+// console.log("Oi," + " tudo" + " bem");
+
+// // 7 - Template Strings
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
+// 8 - Boolean
+// console.log(true);
+// console.log(5 > 20);
+// console.log(30 > 10);
+
+// console.log(typeof false);
+
+// 9 - Comparações
+// console.log(5 >= 5);
+// console.log(5 > 5);
+// console.log(10 == 10);
+// console.log(10 == 9);
+// console.log(10 != 9);
+
+// // 10 - Idêntico
+// console.log(9 === "9");
+// console.log(9 !== "9");
+
+// 11 - Operadores Lógicos
+// console.log(true && true);
+// console.log(true && false);
+// console.log(5 > 2 && 2 < 10);
+// console.log(5 > 2 && `Michel` === 1);
+// console.log(5 > 2 || `Michel` === 1);
+// console.log(5 < 2 || 5 > 100);
+// console.log(!true)
+// console.log(!5 > 2)
+
+// 12 - Empty values
