1057e16b5ced987ac9c3f07340afc84573259f05
Michel Basteiro <michelhfb2006@gmail.com>
2024-02-08 15:50:04 -0300

    Aprendendo estruturas de programação
    Aprendendo loops e condicionais

---
+++ JS/3_ESTRUTURAS/js/script.js
@@ -0,0 +1,138 @@
+// 1 - Variáveis
+// let nome = `Michel`;
+// console.log(nome);
+
+// nome = `Michel Henrique`;
+// console.log(nome);
+
+// const idade = 17;
+// console.log(idade);
+// // idade = 18; // Isso não é correto
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+// 2 - Mais sobre variáveis 
+// let 2teste = `invalido`
+// let @teste = `inavlido`
+
+// let a = 10, 
+// b = 20, 
+// c = 30
+// console.log(a, b, c);
+
+// const nomecompleto = `Michel Henrique`;
+// const nomeCompleto = `Michel Henrique`;
+
+// console.log(nomeCompleto);
+// console.log(nomecompleto);
+
+// let _teste = `ok`;
+// let $teste = `ok`;
+
+// console.log(_teste, $teste)
+
+// 3 - Prompt
+// const age = prompt(`Digite a sua idade:`);
+// console.log(`Você tem ${age} anos.`);
+
+// 4 - Alert 
+// alert(`Testando!!!!!`);
+// const z = 10;
+// alert(`O número é ${z}`);
+
+// 5 - Math
+// console.log(Math.max(5, 2, 1, 10));
+// console.log(Math.floor(5.14));
+// console.log(Math.ceil(5.14));
+
+// 6 - Console
+// console.log(`Teste!`);
+// console.error(`Erro!`);
+// console.warn(`Aviso!`);
+
+// 7 - If
+// const m = 10;
+
+// if (m > 5) {
+//     console.log(`M é maior que 5!`);
+// } 
+
+// const user = `João`
+
+// if (user === `João`) {
+//     console.log(`O nome do usuario é João!`);
+// }
+
+// if (user === `Maria`) {
+//     console.log(`Olá Maria!`);
+// }
+
+// console.log(user === `João`, user === `Maria`);
+
+// 8 - Else
+// const loggedIn = false
+
+// if (loggedIn) {
+//     console.log(`Esta autenticado`);
+// } else {
+//     console.log(`Não esta autenticado`)
+// }
+
+// const q = 10
+// const w = 15
+
+// if(q > 5 && w > 20) {
+//     console.log(`Números mais altos`)
+// } else {
+//     console.log(`Os números inseridos não são mais altos`)
+// }
+
+// 9 - Else if 
+// if (1 > 2) {
+//     console.log(`Teste`)
+// } else if (2 > 3) {
+//     console.log(`Teste 2`)
+// } else if (5 > 1) {
+//     console.log(`Agora é verdadeiro!`)
+// }
+
+// const userName = `Michel`
+// const userAge = 17
+
+// if (userName === `José`) {
+//     console.log(`Bem vindo José!`)
+// } else if (userName === `Michel` && userAge === 17) {
+//     console.log(`Bem vindo Michel!`)
+// } else {
+//     console.log(`Nenhuma condição aceita!`)
+// }
+
+// 10 - While
+// let p = 0
+
+// while (p < 5) {
+//     console.log(`Repetindo o valor de p: ${p}`)
+//     p++
+// }
+
+// 11 - Do while
+// let o = 10
+
+// do {
+//     console.log(`Valor de o: ${o}`)
+//     o--
+// } while (o > 11)
+
+// 12 - For
+// for(let t = 0; t < 10; t++) {
+//     console.log(`Esse é o valor de ${t}`)
+// }
+
+// let r = 10
+// for(r; r > 0; r--) {
+//     console.log(`Algo esta se repetiu ${r} vezes`)
+// }
+
+// console.log(r);
+
