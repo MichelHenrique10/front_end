ff38eefc287a0904d54a8585bcfd6c877fa30704
Michel Basteiro <michelhfb2006@gmail.com>
2024-02-26 10:43:52 -0300

    Finalizando a sessão de OOP
    Finalizando as aulas de orientação a objetos

--- JS/6_OOP/js/scripts.js
+++ JS/6_OOP/js/scripts.js
@@ -156,20 +156,69 @@
 // console.log(Humano.prototype.idade);
 
 // 12 - Symbol
-class Aviao {
-    constructor(marca, turbina) {
-        this.marca = marca
-        this.turbina = turbina
+// class Aviao {
+//     constructor(marca, turbina) {
+//         this.marca = marca
+//         this.turbina = turbina
+//     }
+// }
+
+// const asas = Symbol();
+// Aviao.prototype[asas] = 2;
+// const pilotos = Symbol();
+// Aviao.prototype[pilotos] = 3;
+
+// const boeing = new Aviao(`Boeing`, 10);
+// console.log(boeing);
+
+// console.log(boeing[asas]);
+// console.log(boeing[pilotos]);
+
+// 13 - Getter e setters
+class Post {
+    constructor(titulo, descricao, tags) {
+        this.titulo = titulo
+        this.descricao = descricao
+        this.tags = tags
+    }
+
+    get exibirTitulo() {
+        return `Você está lendo: ${this.titulo}`;
+    }
+
+    set adicionarTags(tags) {
+        const tagsArrays = tags.split(`, `)
+        this.tags = tagsArrays
     }
 }
 
-const asas = Symbol();
-Aviao.prototype[asas] = 2;
-const pilotos = Symbol();
-Aviao.prototype[pilotos] = 3;
+const myPost = new Post(`Algum Titulo`, `É um post sobre programação`);
+console.log(myPost);
+console.log(myPost.exibirTitulo);
 
-const boeing = new Aviao(`Boeing`, 10);
-console.log(boeing);
+myPost.adicionarTags = `programação, javascript, js`;
+console.log(myPost);
 
-console.log(boeing[asas]);
-console.log(boeing[pilotos]);
+// 14 - Herança
+class Mamifero {
+    constructor(patas) {
+        this.patas = patas
+    };
+};
+
+class Lobo extends Mamifero {
+    constructor(patas, nome) {
+        super(patas, patas)
+        this.nome = nome
+    }
+}
+
+const shark = new Lobo(4, `Shark`);
+console.log(shark);
+console.log(shark.patas);
+
+// 15 - InstanceOf
+console.log(shark instanceof Lobo);
+console.log(Lobo instanceof Mamifero);
+console.log(new Lobo(4, `Teste`) instanceof Mamifero);
+console.log(new Post (`a`, `b`) instanceof Lobo);
