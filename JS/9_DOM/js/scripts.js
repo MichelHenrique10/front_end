470c546c24f27ac13a40790c96d1e801309e3017
Michel Basteiro <michelhfb2006@gmail.com>
2024-03-02 11:13:04 -0300

    Finalizando sessão do DOM
    Finalizando a sessão de DOM e aprendendo a manipular o HTML e CSS através do JS

--- JS/9_DOM/js/scripts.js
+++ JS/9_DOM/js/scripts.js
@@ -1,68 +1,90 @@
 // 1 - Movendo-se pelo DOM
-console.log(document.body);
+// console.log(document.body);
 
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes);
 
-console.log(document.body.childNodes[1].childNodes);
+// console.log(document.body.childNodes[1].childNodes);
 
-console.log(document.body.childNodes[1].childNodes[1].textContent);
+// console.log(document.body.childNodes[1].childNodes[1].textContent);
 
-// 2 - Selecionando elementos por tag
+// // 2 - Selecionando elementos por tag
 const listItens = document.getElementsByTagName(`li`);
 console.log(listItens); 
 
-// 3 - Selecionando elementos por Id
-const title = document.getElementById(`title`);
-console.log(title);
+// // 3 - Selecionando elementos por Id
+// const title = document.getElementById(`title`);
+// console.log(title);
 
-// 4 - Selecionando elementos por Classes
+// // 4 - Selecionando elementos por Classes
 const products = document.getElementsByClassName(`product`);
 console.log(products);
 
-// 5 - Selecionando elementos por CSS
-const productsQuery = document.querySelectorAll(`.product`);
-console.log(productsQuery);
+// // 5 - Selecionando elementos por CSS
+// const productsQuery = document.querySelectorAll(`.product`);
+// console.log(productsQuery);
 
 const mainContainer = document.querySelector(`#main-container`);
 console.log(mainContainer);
 
-// 6 - insertBefore
-const p = document.createElement(`p`);
-console.log(p);
+// // 6 - insertBefore
+// const p = document.createElement(`p`);
+// console.log(p);
 
-const header = title.parentElement;
-console.log(header);
+// const header = title.parentElement;
+// console.log(header);
 
-header.insertBefore(p, title);
+// header.insertBefore(p, title);
 
-// 7 - appendChild
-const navLinks = document.querySelector(`nav ul`);
+// // 7 - appendChild
+// const navLinks = document.querySelector(`nav ul`);
 
-const li = document.createElement(`li`);
+// const li = document.createElement(`li`);
 
-navLinks.appendChild(li);
+// navLinks.appendChild(li);
 
-// 8 - replaceChild
-const h2 = document.createElement(`h2`);
-h2.textContent = `Meu novo título!`;
-header.replaceChild(h2, title);
+// // 8 - replaceChild
+// const h2 = document.createElement(`h2`);
+// h2.textContent = `Meu novo título!`;
+// header.replaceChild(h2, title);
 
-// 9 - createTextNode
-const myText = document.createTextNode(`Agora vamos colocar mais um titulo`);
-console.log(myText);
+// // 9 - createTextNode
+// const myText = document.createTextNode(`Agora vamos colocar mais um titulo`);
+// console.log(myText);
 
-const h3 = document.createElement(`h3`);
-h3.appendChild(myText);
-console.log(h3);
+// const h3 = document.createElement(`h3`);
+// h3.appendChild(myText);
+// console.log(h3);
 
-mainContainer.appendChild(h3);
+// mainContainer.appendChild(h3);
 
-// 10 - Trabalhando com atributos
-const firstLinks = navLinks.querySelector(`a`);
-console.log(firstLinks);
+// // 10 - Trabalhando com atributos
+// const firstLinks = navLinks.querySelector(`a`);
+// console.log(firstLinks);
 
-firstLinks.setAttribute(`href`, `https://www.google.com`);
+// firstLinks.setAttribute(`href`, `https://www.google.com`);
 
-console.log(firstLinks.getAttribute(`href`));
+// console.log(firstLinks.getAttribute(`href`));
 
-firstLinks.setAttribute(`target`, `_blank`);
+// firstLinks.setAttribute(`target`, `_blank`);
+
+// // 11 - Altura e largura
+// const footer = document.querySelector(`footer`);
+// console.log(footer.offsetHeight);
+// console.log(footer.offsetWidth);
+
+// console.log(footer.clientHeight);
+// console.log(footer.clientWidth);
+
+// 12 - Posição do elemento
+const product1 = products[0];
+console.log(product1.getBoundingClientRect());
+
+// 13 - CSS com JS
+mainContainer.style.color = `Red`;
+mainContainer.style.backgroundColor = `Blue`;
+mainContainer.style.paddingBottom = `5px`
+
+// 14 - Alterando estilos de varios elementos
+for (const li of listItens) {
+    li.style.backgroundColor = `Red`;
+}
